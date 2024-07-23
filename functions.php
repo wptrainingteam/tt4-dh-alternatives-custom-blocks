a5847bb31de698a1a13aa82410a895d46d751a05
JuanMa Garrido <juanma.garrido@gmail.com>
2024-07-23 11:58:01 +0100

    removed PHP registration in favour of json under styles

--- functions.php
+++ functions.php
@@ -17,15 +17,11 @@ define('THEME_STYLE_URL', get_stylesheet_uri());
 define('THEME_DIR', __DIR__);
 define('API_BASE_URL', 'https://api.quotable.io');
 
-require_once THEME_DIR . '/includes/register-block-style.php';
 require_once THEME_DIR . '/includes/register-source-block-binding.php';
 require_once THEME_DIR . '/includes/enqueue-assets.php';
 
 // setup
 add_action('enqueue_block_editor_assets', 'dh__enqueue_block_variations_script');
-
-// hand-drawn-red - Register Block Style via PHP.
-add_action("init", 'dh__register_block_styles');
 
 // Registers a custom block bindings source.
 add_action('init', 'dh__register_block_bindings_source');
