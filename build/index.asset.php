0c66a3c53fee78632f8eaf07a8da1ad4c6a3e416
JuanMa Garrido <juanma.garrido@gmail.com>
2024-07-23 17:17:14 +0100

    enable variations

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '66545036d55ec77ee71d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'd5ac8b5c7ee9a15144e9');
